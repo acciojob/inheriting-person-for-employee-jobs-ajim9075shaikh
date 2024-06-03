a9d4c479be76f96f40d81814b1ffc19f98cc49aa
ajim9075shaikh <142928027+ajim9075shaikh@users.noreply.github.com>
2024-06-03 08:55:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 // complete this js code
-class Person {
-	constructor(name,age){
-		this.name=name;
-		this.age=age;
-	}
-	greet(){
-		 console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-	}
+	class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greet() {
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+    }
 }
+
 class Employee extends Person {
- constructor(name,age,jobTitle){
-	 super(name,age);
-	 this.jobTitle=jobTitle
- }
-	jobGreet(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+    constructor(name, age, jobTitle) {
+        super(name, age);
+        this.jobTitle = jobTitle;
+    }
+
+    jobGreet() {
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+    }
 }
 
 
